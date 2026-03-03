cb596198d077c9e8b319a40a24f56993862feaaf
rosasguerrerozuleimayamileth-cell <rosasguerrerozuleimayamileth@gmail.com>
2026-03-02 18:52:34 -0600

    Add files via upload

---
+++ clase estudiante.py
@@ -0,0 +1,10 @@
+class Estudiante: 
+    //Atributos
+    def_int_(nombre,edad,calificacion):
+    string nombre = {Yamileth}
+    int edad = {16 años}
+    string calificcion = {9}
+    //Metodo
+Estudiante1 = Estudiante("Yamileth",16,9)
+Estudiante2 = Estudiante mostrar_datos();
+
